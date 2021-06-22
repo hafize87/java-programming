920c57ecbfb5b476302a7ed082bf74b1b78790be
Hafize Aksoy <hafize.teacher@gmail.com>
2021-06-22 16:22:44 -0700

    Intro

---
+++ src/day56_abstraction/Greeting.java
@@ -0,0 +1,8 @@
+package day56_abstraction;
+
+public interface Greeting {
+
+    void hi();
+    void bye();
+
+}
---
+++ src/day56_abstraction/MountainLanguage.java
@@ -0,0 +1,14 @@
+package day56_abstraction;
+
+public class MountainLanguage implements Greeting {
+
+    @Override
+    public void hi() {
+
+    }
+
+    @Override
+    public void bye() {
+
+    }
+}
