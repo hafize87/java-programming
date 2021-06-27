d613c73d223f00ddb388a01169cbd23040b4aa44
Hafize Aksoy <hafize.teacher@gmail.com>
2021-06-27 09:29:39 -0700

    Abstraction interface

--- src/day57_abstraction_polymorphism/abstract_class_vs_interface/AbstractA.java
+++ src/day57_abstraction_polymorphism/abstract_class_vs_interface/AbstractA.java
@@ -1,4 +1,15 @@
 package day57_abstraction_polymorphism.abstract_class_vs_interface;
 
 public abstract class AbstractA {
+
+    public abstract void absMethodA();
+
+    public void methodB(){
+
+        System.out.println("methodB called");
+    }
+
+    public static void staticC(){
+        System.out.println("static methodC is called");
+    }
 }
---
+++ src/day57_abstraction_polymorphism/abstract_class_vs_interface/ConcreteA.java
@@ -0,0 +1,22 @@
+package day57_abstraction_polymorphism.abstract_class_vs_interface;
+
+public class ConcreteA extends AbstractA implements InterfaceA, InterfaceB {
+
+    public void absMethodA(){
+
+    }
+
+
+    @Override
+    public void methodB(){
+
+        System.out.println("methodB overridden version is called");
+    }
+
+    // @Override   --> static method cannot overridden , it can hidden
+    public static void staticC(){
+        System.out.println("static methodC is called");
+    }
+
+
+}
