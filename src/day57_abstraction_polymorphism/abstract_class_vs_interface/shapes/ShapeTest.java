b2e2d32125cbd8a2616a53fb1e724846989e5294
Hafize Aksoy <hafize.teacher@gmail.com>
2021-06-27 13:58:57 -0700

    Abstraction interface

--- src/day57_abstraction_polymorphism/abstract_class_vs_interface/shapes/ShapeTest.java
+++ src/day57_abstraction_polymorphism/abstract_class_vs_interface/shapes/ShapeTest.java
@@ -1,5 +1,8 @@
 package day57_abstraction_polymorphism.abstract_class_vs_interface.shapes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShapeTest {
     public static void main(String[] args) {
 
@@ -12,5 +15,13 @@ public class ShapeTest {
 
         Shape square = new Square();
         square.draw();
+
+        List<Shape> shapes = new ArrayList<>();
+        shapes.add(triangle);
+        shapes.add(new Triangle());
+        shapes.add(new Circle());
+        shapes.add(new Square());
+
+
     }
 }
