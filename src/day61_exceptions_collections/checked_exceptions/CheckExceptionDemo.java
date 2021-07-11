f7742b61b0ed07437530f28ffa5dae724b125b5e
Hafize Aksoy <hafize.teacher@gmail.com>
2021-07-11 14:11:00 -0700

    exception

---
+++ src/day61_exceptions_collections/checked_exceptions/CheckExceptionDemo.java
@@ -0,0 +1,24 @@
+package day61_exceptions_collections.checked_exceptions;
+
+public class CheckExceptionDemo {
+    public static void main(String[] args) throws InterruptedException {
+
+        // 1 way - Handling using try catch
+        System.out.println("About to sleep for 5 seconds");
+
+        try {
+            Thread.sleep(1000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+
+        System.out.println("woke up after 5 second");
+
+
+        //2 way - Declaring using throws keyword
+
+        Thread.sleep(1000);  // yukariya method seklinde yaziyorsun  -- >  throws InterruptedException
+    }
+
+
+}
