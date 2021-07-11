d470bbdde690ad040d45c2c4f6b32776baee6de5
Hafize Aksoy <hafize.teacher@gmail.com>
2021-07-11 08:34:35 -0700

    exception

---
+++ src/day61_exceptions_collections/ElementaryStudentsTest.java
@@ -0,0 +1,11 @@
+package day61_exceptions_collections;
+
+public class ElementaryStudentsTest {
+    public static void main(String[] args) {
+
+        ElementarySchool el1 = new ElementarySchool();
+        el1.setName("bob");
+        el1.setAge(1);
+        System.out.println(el1.toString());
+    }
+}
