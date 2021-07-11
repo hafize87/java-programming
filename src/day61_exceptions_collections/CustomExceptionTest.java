5ea7c3f322f98a439747430aa8cc0b19f7739996
Hafize Aksoy <hafize.teacher@gmail.com>
2021-07-11 12:30:10 -0700

    exception

---
+++ src/day61_exceptions_collections/CustomExceptionTest.java
@@ -0,0 +1,30 @@
+package day61_exceptions_collections;
+
+public class CustomExceptionTest {
+    public static void main(String[] args) {
+
+        int minutes = 60;
+        System.out.println("Class is going on for " + minutes + " minutes.");
+
+        if(minutes > 70){
+
+            throw new BreakTimeException("it is break time");
+        }
+
+        System.out.println("lets continue the class");
+
+
+      //  -----------------------------------
+
+            double balance = 400.0;
+            double items = 600.0;
+
+        System.out.println("i have $" + balance + " and adding purchasing item for $" + items);
+
+            if(items > balance){
+
+                throw new InsufficientBalanceException("not enough money");
+            }
+        System.out.println("Item successfully purchased");
+    }
+}
--- src/day61_exceptions_collections/ElementaryStudentsTest.java
+++ src/day61_exceptions_collections/ElementaryStudentsTest.java
@@ -3,9 +3,22 @@ package day61_exceptions_collections;
 public class ElementaryStudentsTest {
     public static void main(String[] args) {
 
-        ElementarySchool el1 = new ElementarySchool();
-        el1.setName("bob");
-        el1.setAge(1);
-        System.out.println(el1.toString());
+
+
+        try {
+            ElementarySchool el1 = new ElementarySchool();
+            el1.setName("bob");
+            el1.setAge(1);
+            System.out.println(el1.toString());
+
+        }catch (IllegalArgumentException e){
+            e.printStackTrace();  // print error stack trace and continue. does not stop code.
+        }
+
+        System.out.println("--- Execution completed ---");
+
+
+
+
     }
 }
